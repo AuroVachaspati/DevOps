8d01ec046c07cfc2075585d7d3d3f36e1c045ece
AuroVachaspati <aurovachaspathi@gmail.com>
2024-08-26 19:21:50 +0530

    variables

---
+++ git-practice/03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=Ramesh
+PERSON2=Suresh
+
+echo "$PERSON1 :: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1. Im good. what about you?"
+echo "$PERSON1:: Im good. Whats up?"
+echo "$PERSON2 :: I started learning Shell scripting $PERSON1"
