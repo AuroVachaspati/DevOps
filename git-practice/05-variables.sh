c591fec810680055f0549d2c7bcea9ec8f2abadd
AuroVachaspati <aurovachaspathi@gmail.com>
2024-08-26 19:50:10 +0530

    I gave two arguments

--- git-practice/05-variables.sh
+++ git-practice/05-variables.sh
@@ -6,3 +6,9 @@ echo "Please enter your username :: "
 read  -s USERNAME #takes input into USERNAME variable
 
 echo "Username entered is $USERNAME"
+
+echo "Please enter your Password:: "
+
+read -s PASSWORD
+echo "Paswword entered is $PASSWORD"
+
