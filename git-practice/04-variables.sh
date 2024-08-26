6c88bff13732d15603604101b393262d58127e37
AuroVachaspati <aurovachaspathi@gmail.com>
2024-08-26 19:32:06 +0530

    I gave two arguments

---
+++ git-practice/04-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1 :: Hi $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1. Im good. what about you?"
+echo "$PERSON1:: Im good. Whats up?"
+echo "$PERSON2 :: I started learning Shell scripting $PERSON1"
