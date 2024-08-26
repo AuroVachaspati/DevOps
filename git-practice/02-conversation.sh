291625923eaa20bef8c3cdc64184f85645f52558
AuroVachaspati <aurovachaspathi@gmail.com>
2024-08-26 19:01:09 +0530

    Added conversation script

---
+++ git-practice/02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh :: Hi Suresh, How are you?"
+echo "Suresh:: Hi Rames. Im good. what about you?"
+echo "Ramesh:: Im good. Whats up?"
+echo "I started learning Shell scripting"
