5d950784ef726b704c382c2bbe9842c322a35be9
AuroVachaspati <aurovachaspathi@gmail.com>
2024-08-27 22:19:02 +0530

    command output

--- git-practice/09-special-vars.sh
+++ git-practice/09-special-vars.sh
@@ -6,4 +6,6 @@ echo "Script name is $0"
 echo "Current working directory is $PWD"
 echo "Home directory of current user: $HOME"
 echo "PID Of running shell script: $$"
+sleep 100 &
+echo "PID of last background command: $!"
 
