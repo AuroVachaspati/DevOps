03e1d643e58edae1d0ea2291281d126da6838563
AuroVachaspati <aurovachaspathi@gmail.com>
2024-08-27 22:56:58 +0530

    command output

--- git-practice/11-mysql-installation.sh
+++ git-practice/11-mysql-installation.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-USERID=$( id -u)
-echo "User ID is: $USER"
+USERID=$(id -u)
+#echo "User ID is: $USER"
+if [ $USER -ne 0 ]
+then
+    echo "Please run this scipt with root previleges"
+fi
+
+dnf install git -y
