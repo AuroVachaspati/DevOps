fb764a10d4de6928c5e5030d2f6892e1a0476df0
AuroVachaspati <aurovachaspathi@gmail.com>
2024-08-28 21:44:12 +0530

    conditionals

--- git-practice/11-mysql-installation.sh
+++ git-practice/11-mysql-installation.sh
@@ -1,23 +1,38 @@
-#!/bin/bash
-
-
-
 USERID=$(id -u)
-#echo "User ID is: $USERID"
 
-if [ $USERID -ne 0 ]
-then
+if [ $USERID -ne 0 ]; then
     echo "Please run this script with root privileges"
     exit 1  # Exit the script if not running as root
 fi
 
-dnf list installed git
+# Check if Git is installed
+dnf list installed git &> /dev/null
 
-if [ $? ne 0 ]
-then
-    echo "Git is not installed, going to install.."
+if [ $? -ne 0 ]; then
+    echo "Git is not installed, going to install..."
     dnf install git -y
+    if [ $? -ne 0 ]; then
+        echo "Failed to install Git, check for errors"
+        exit 1
+    else
+        echo "Git installed successfully"
+    fi
 else
     echo "Git is already installed"
 fi
 
+# Check if MySQL is installed
+dnf list installed mysql &> /dev/null
+
+if [ $? -ne 0 ]; then
+    echo "MySQL is not installed, going to install..."
+    dnf install mysql -y
+    if [ $? -ne 0 ]; then
+        echo "Failed to install MySQL, check for errors"
+        exit 1
+    else
+        echo "MySQL installed successfully"
+    fi
+else
+    echo "MySQL is already installed"
+fi
