0baf605123b393bca36f97a63dc85efedc108808
AuroVachaspati <aurovachaspathi@gmail.com>
2024-08-28 23:04:39 +0530

    conditionals

--- git-practice/11-mysql-installation.sh
+++ git-practice/11-mysql-installation.sh
@@ -6,7 +6,13 @@ USERID=$(id -u)
 #echo "User ID is: $USERID"
 
 VALIDATE(){
-    echo "exit status: $1"
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 is... FAILED"
+        exit 1
+    else
+        echo "$2 is....SUCCESS"
+    fi        
 }
 
 if [ $USERID -ne 0 ]
@@ -15,17 +21,22 @@ then
     exit 1  # Exit the script if not running as root
 fi
 
+
+
+
+
 dnf list installed git
 
 if [ $? -ne 0 ]
 then
     echo "Git is not installed, going to install.."
     dnf install git -y
+    VALIDATE $? "Installing git"
 else
     echo "Git is already installed"
 fi
 
-VALIDATE $?
+
 
 # dnf list installed mysql
 
