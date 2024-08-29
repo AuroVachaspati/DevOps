d4b58f4b291a769189081da257191c94e88943d3
AuroVachaspati <aurovachaspathi@gmail.com>
2024-08-29 23:26:37 +0530

    conditionals

--- git-practice/15-loops.sh
+++ git-practice/15-loops.sh
@@ -1,38 +1,47 @@
 #!/bin/bash
 
-# USERID=$(id -u)
-# echo "UserID is: $USERID"  # Debug line to check the value of USERID
+USERID=$(id -u)
+echo "UserID is: $USERID"  # Debug line to check the value of USERID
 
-# R="\e[31m"
-# G="\e[32m"
-
-
-# CHECK_ROOT(){
-#     if [ $USERID -ne 0 ]
-#     then
-#         echo "Please run this script with root privileges"
-#         exit 1  # Exit the script if not running as root
-#     fi
-# }
+R="\e[31m"
+G="\e[32m"
 
 
+CHECK_ROOT(){
+    if [ $USERID -ne 0 ]
+    then
+        echo "Please run this script with root privileges"
+        exit 1  # Exit the script if not running as root
+    fi
+}
 
 
-# VALIDATE(){
-#     if [ $1 -ne 0 ]
-#     then 
-#         echo "$2 is... $R FAILED $N"
-#         exit 1
-#     else
-#         echo "$2 is....$G SUCCESS $N"
-#     fi        
-# }
 
-# CHECK_ROOT
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then 
+        echo "$2 is... $R FAILED $N"
+        exit 1
+    else
+        echo "$2 is....$G SUCCESS $N"
+    fi        
+}
+
+CHECK_ROOT
 
 
 # 15-loops.sh git mysql, nginix, etc..
 for package in $@ #all arguments passed to it
 do 
-    echo $package
+   dnf list installed $package
+
+    if [ $? -ne 0 ]
+    then
+        echo  "$package is not installed, going to install.."
+        dnf install $package -y
+        VALIDATE $? "Installing $package"
+    else
+        echo " $package is already installed"
+    fi
 done
