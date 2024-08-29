4ded5e1f32b41d9eac4d0c11ea4e530923b37c65
AuroVachaspati <aurovachaspathi@gmail.com>
2024-08-29 23:15:07 +0530

    conditionals

--- git-practice/15-loops.sh
+++ git-practice/15-loops.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-USERID=$(id -u)
-echo "UserID is: $USERID"  # Debug line to check the value of USERID
+# USERID=$(id -u)
+# echo "UserID is: $USERID"  # Debug line to check the value of USERID
 
-R="\e[31m"
-G="\e[32m"
-
-
-CHECK_ROOT(){
-    if [ $USERID -ne 0 ]
-    then
-        echo "Please run this script with root privileges"
-        exit 1  # Exit the script if not running as root
-    fi
-}
+# R="\e[31m"
+# G="\e[32m"
 
 
+# CHECK_ROOT(){
+#     if [ $USERID -ne 0 ]
+#     then
+#         echo "Please run this script with root privileges"
+#         exit 1  # Exit the script if not running as root
+#     fi
+# }
 
 
-VALIDATE(){
-    if [ $1 -ne 0 ]
-    then 
-        echo "$2 is... $R FAILED $N"
-        exit 1
-    else
-        echo "$2 is....$G SUCCESS $N"
-    fi        
-}
 
-CHECK_ROOT
+
+# VALIDATE(){
+#     if [ $1 -ne 0 ]
+#     then 
+#         echo "$2 is... $R FAILED $N"
+#         exit 1
+#     else
+#         echo "$2 is....$G SUCCESS $N"
+#     fi        
+# }
+
+# CHECK_ROOT
 
 
 # 15-loops.sh git mysql, nginix, etc..
